abf3d2ce091cd39e29c07025744e9769329c126a
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-04-01 15:01:30 +0700

    feat: update setup script

--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,9 @@
-pip install ./table-detection
-pip install ./table-recognition
-pip install ./table-detection
-pip install ./table-recognition
+echo "Installing table-detection package!"
+pip install -q ./table-detection
+echo "Installing table-recognition package!"
+pip install -q ./table-recognition
+echo "Installing text-detection package!"
+pip install -q ./text-detection
+echo "Installing text-recognition package!"
+pip install -q ./text-recognition
+echo "Completed!"
