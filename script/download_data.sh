fd0053bc858a02879e961f4043ec8da4acaafb0e
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-04-01 15:09:27 +0700

    build: update package, add download script

---
+++ script/download_data.sh
@@ -0,0 +1,9 @@
+curl -L \
+"https://drive.usercontent.google.com/download?\
+id=1T0cmkhTgu3ahyMIwGZeby612RpVdDxOR&\
+export=download&authuser=0&\
+confirm=t&\
+uuid=05de46a8-e5fb-4732-b832-c0bbf146bc2c"\
+> ocr_dataset.zip
+
+unzip -q ocr_dataset.zip -d data/
