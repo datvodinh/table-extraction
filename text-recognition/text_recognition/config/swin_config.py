074ec18aaa8764148554ca510650f06e61d3268f
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-04-10 11:01:34 +0700

    feat: update swin transformer model

--- text-recognition/text_recognition/config/swin_config.py
+++ text-recognition/text_recognition/config/swin_config.py
@@ -4,27 +4,8 @@ from dataclasses import dataclass
 
 @dataclass
 class SwinTransformerOCRConfig:
-    # MODEL
     in_channels: int = 3
-
-    # ENCODER
-    patch_size: tuple[int] = (4, 4)
-    embed_dim: int = 96
-    window_size: tuple[int] = (8, 8)
-    depths: tuple[int] = (2, 12, 2)
-    num_heads: tuple[int] = (6, 12, 24)
-    # DECODER
-    d_model: int = 384
-    decoder_layers: int = 6
-    decoder_attention_heads: int = 6
-    decoder_ffn_dim: int = 1536
-    scale_embedding: bool = False
-    dropout: float = 0.1
-    use_learned_position_embeddings: bool = True
-    activation_function: str = "silu"
-    # INFERENCE
     max_tokens: int = 128
-    # DATAMODULE
     num_workers: int = 2
     lr: float = 0.0003
     betas: tuple[float] = (0.9, 0.98)
--- text-recognition/text_recognition/model/swin.py
+++ text-recognition/text_recognition/model/swin.py
@@ -3,40 +3,37 @@ import torch.nn as nn
 import pytorch_lightning as pl
 from einops import rearrange
 from torch.optim.lr_scheduler import OneCycleLR
-from transformers import VisionEncoderDecoderModel
+from transformers import (
+    VisionEncoderDecoderModel,
+    Swinv2ForImageClassification,
+    Swinv2Config
+)
 from text_recognition.config import SwinTransformerOCRConfig
 from text_recognition.tokenizer import OCRTokenizer
-from torchvision.models.swin_transformer import (
-    SwinTransformer,
-    SwinTransformerBlockV2,
-    PatchMergingV2
-)
 
 
 class SwinTransformerEncoder(nn.Module):
     def __init__(self, config: SwinTransformerOCRConfig):
         super().__init__()
-        self.model = SwinTransformer(
-            patch_size=config.patch_size,
-            embed_dim=config.embed_dim,
-            depths=config.depths,
-            num_heads=config.num_heads,
-            window_size=config.window_size,
-            dropout=config.dropout,
-            block=SwinTransformerBlockV2,
-            downsample_layer=PatchMergingV2
+        config_pretrained = Swinv2Config.from_pretrained("microsoft/swinv2-tiny-patch4-window8-256")
+        config_pretrained.image_size = config.img_size
+        config_pretrained.num_channels = config.in_channels
+        config_pretrained.num_labels = 0
+        self.model = Swinv2ForImageClassification.from_pretrained(
+            "microsoft/swinv2-tiny-patch4-window8-256",
+            config=config_pretrained
         )
-
-        # Avoid unused params in DDP
-        self.model.permute = None
-        self.model.avgpool = None
-        self.model.flatten = None
-        self.model.head = None
+        self.model.swinv2.embeddings.patch_embeddings.projection = nn.Conv2d(3, 192, kernel_size=(4, 4), stride=(4, 4))
+        self.model.swinv2.embeddings.patch_embeddings.norm = nn.LayerNorm((192,), eps=1e-05, elementwise_affine=True)
+        self.model.swinv2.embeddings.norm = nn.LayerNorm((192,), eps=1e-05, elementwise_affine=True)
+        self.model.swinv2.encoder.layers.pop(0)
+        self.conv = nn.Conv2d(768, 384, 1)
 
     def forward(self, x):
-        x = self.model.features(x)  # B H W C
-        x = self.model.norm(x)     # B H W C
-        return rearrange(x, "b h w c -> b (h w) c")
+        x_out = self.model(
+            x, output_hidden_states=True
+        ).reshaped_hidden_states[-1]  # last hidden states
+        return rearrange(self.conv(x_out), "b c h w -> b (h w) c")
 
 
 class TrOCRDecoder(nn.Module):
