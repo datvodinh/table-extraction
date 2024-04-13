ac85a3ff9bae0ac92f1034791f2fbf52bf9751a3
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-04-13 11:55:31 +0700

    feat: add inference pipeline for text recognition

---
+++ text-recognition/text_recognition/inference/pipeline.py
@@ -0,0 +1,36 @@
+import torch
+import cv2
+import numpy as np
+from PIL.Image import Image
+from text_recognition.datamodule import OCRTransform
+from text_recognition.model import TextRecognitionModel
+
+
+class TextRecognitionInference:
+    def __init__(self, ckpt_path: str) -> None:
+        self.model = TextRecognitionModel.from_path(ckpt_path)
+        self.model.eval()
+        self.transform = OCRTransform(img_size=self.model.config.img_size, stage="val")
+
+    def _predict_from_path(self, img_path: str):
+        image = cv2.imread(img_path)
+        return self._from_numpy(image)
+
+    def _predict_from_pil_image(self, img: Image):
+        image = np.asarray(img)
+        return self._from_numpy(image)
+
+    def _predict_from_numpy(self, img: np.ndarray):
+        image = self.transform(image=img).unsqueeze(0)
+        return self.model.predict(image)
+
+    @torch.no_grad()
+    def predict(self, data: str | Image | np.ndarray):
+        if isinstance(data, str):
+            return self._predict_from_path(data)
+        elif isinstance(data, Image):
+            return self._predict_from_pil_image(data)
+        elif isinstance(data, np.ndarray):
+            return self._predict_from_numpy(data)
+        else:
+            raise ValueError("Invalid data!")
--- text-recognition/text_recognition/model/model.py
+++ text-recognition/text_recognition/model/model.py
@@ -1,4 +1,4 @@
-from typing import Any
+import torch
 from .swin import SwinTransformerOCR
 from .vit import VisionTransformerOCR
 from text_recognition.config import SwinTransformerOCRConfig, VisionTransformerOCRConfig
@@ -7,10 +7,25 @@ from text_recognition.config import SwinTransformerOCRConfig, VisionTransformerO
 class TextRecognitionModel:
 
     @staticmethod
-    def from_config(config: Any):
+    def from_config(config: SwinTransformerOCRConfig | VisionTransformerOCRConfig):
         if isinstance(config, SwinTransformerOCRConfig):
             return SwinTransformerOCR(config)
         elif isinstance(config, VisionTransformerOCRConfig):
             return VisionTransformerOCR(config)
         else:
             raise ValueError("Invalid config!")
+
+    @staticmethod
+    def from_path(ckpt_path: str):
+        try:
+            config = torch.load(
+                ckpt_path, map_location=torch.device('cpu')
+            )['hyper_parameters']['config']
+            if isinstance(config, SwinTransformerOCRConfig):
+                return SwinTransformerOCR.load_from_checkpoint(ckpt_path)
+            elif isinstance(config, VisionTransformerOCRConfig):
+                return VisionTransformerOCR.load_from_checkpoint(ckpt_path)
+            else:
+                raise ValueError("Invalid config!")
+        except ValueError:
+            raise ValueError("Wrong Checkpoint Path!")
